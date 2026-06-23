c176c1654699ced227d2e9d89938e3421145105f
Vyronxoder <114700391+Vyronxoder@users.noreply.github.com>
2026-06-23 17:08:16 +0530

    Import WebDriverWait and ExpectedConditions for tests
    Added WebDriverWait and ExpectedConditions imports for better synchronization in tests.

--- src/main/java/pages/InventoryPage.java
+++ src/main/java/pages/InventoryPage.java
@@ -3,6 +3,9 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
 
 public class InventoryPage {
 
@@ -81,3 +84,4 @@ public class InventoryPage {
             return 0; 
         }
     }
+}
